202fb304d2bb91db6d59ad0750bbfd55b63152a1
Aldito48 <alditofayyadh0.com>
2024-12-06 16:08:17 +0700

    del-confirm

--- layout/notify.php
+++ layout/notify.php
@@ -32,26 +32,4 @@
 
         return;
     }
-
-    function confirmation ($icon, $title, $text, $redirect) {
-        echo "<script>
-            document.addEventListener('DOMContentLoaded', function() {
-                Swal.fire({
-                    icon: '$icon',
-                    title: '$title',
-                    text: '$text',
-                    showDenyButton: false,
-                    showCancelButton: true,
-                    confirmButtonText: 'Save'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        Swal.fire('Saved!', '', 'success');
-                        window.location = '$redirect';
-                    }
-                });
-            });
-        </script>";
-
-        return;
-    }
 ?>
